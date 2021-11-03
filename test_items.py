199f87a1cedc4b71a3ff463eed0ed264fb694949
mitek <bobdubov@yandex.ru>
2021-11-03 11:51:27 +0300

    first commit

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
-def test_5(browser):
+def test__guest_should_see_basket_button(browser):
     browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     browser.implicitly_wait(10)
-    assert browser.find_element_by_class_name('btn-add-to-basket').text == "Añadir al carrito"
+    assert browser.find_element_by_class_name('btn-add-to-basket')
     time.sleep(30)
